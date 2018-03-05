54ddc35cfbc3fe217245e69c81ec46f490e21dcf
Rudy <Rudy@asamco.com>
2018-03-05 16:33:39 +0100

    loading files dynamically

--- VNEngine/Startup.cs
+++ VNEngine/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using VNEngine.Engine;
@@ -18,6 +20,7 @@ namespace VNEngine
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			MainForm form = new MainForm();
+			Environment.CurrentDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "");
 			EngineControl engine = new EngineControl(form);
 			//Game testGame = new Game(engine);
 			Novel testNovel = new Novel(engine);
